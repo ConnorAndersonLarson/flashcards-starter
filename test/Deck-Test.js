4d583c2d37cd5b7bc8c483c513fa7bc77e4fd1d2
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-02-02 20:06:03 -0600

    Pass Deck Tests

--- test/Deck-Test.js
+++ test/Deck-Test.js
@@ -6,12 +6,12 @@ const Card = require('../src/Card');
 
 describe('Deck', function () {
 
-  xit('should be an instance of Deck', function() {
+  it('should be an instance of Deck', function() {
     const deck = new Deck;
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  xit('should initialize with an array of card objects', function() {
+  it('should initialize with an array of card objects', function() {
     const card1 = new Card(1, 'What is your name?', ['Sir Lancelot', 'Sir Galahad', 'King Arthur'], 'Sir Lancelot');
     const card2 = new Card(2, 'What is your quest?', ['To go to the store', 'To eat cheese', 'I seek the Grail'], 'I seek the Grail');
     const card3 = new Card(3, 'What is your favorite color?', ['Blue', 'Yellow', 'Red'], 'Yellow');
@@ -19,7 +19,7 @@ describe('Deck', function () {
     expect(deck.cards.length).to.deep.equal(3)
   })
 
-  xit('should be able to return the amount of card objects', function() {
+  it('should be able to return the amount of card objects', function() {
     const card1 = new Card(1, 'What is your name?', ['Sir Lancelot', 'Sir Galahad', 'King Arthur'], 'Sir Lancelot');
     const card2 = new Card(2, 'What is your quest?', ['To go to the store', 'To eat cheese', 'I seek the Grail'], 'I seek the Grail');
     const card3 = new Card(3, 'What is your favorite color?', ['Blue', 'Yellow', 'Red'], 'Yellow');
