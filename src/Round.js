9d6c27c33903f58c64a77be67070447af6accaea
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-02-04 06:57:04 -0600

    Pass round.takeTurn tests

--- src/Round.js
+++ src/Round.js
@@ -1,9 +1,28 @@
+const Turn = require('../src/Turn')
+
 class Round {
   constructor(deck) {
     this.deck = deck;
     this.turns = 0;
     this.incorrectGuesses = [];
   }
+
+  returnCurrentCard() {
+    return this.deck.cards[0]
+  }
+
+  takeTurn(guess) {
+    const turn = new Turn(guess, this.returnCurrentCard())
+    this.turns++
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.returnCurrentCard())
+    }else{
+      this.deck.cards.shift()
+    }
+    return(turn.giveFeedback())
+  }
+
+
 }
 
 module.exports = Round;
--- test/Round-Test.js
+++ test/Round-Test.js
@@ -10,51 +10,51 @@ describe('Round', function() {
   const card1 = new Card(1, 'What is your name?', ['Sir Lancelot', 'Sir Galahad', 'King Arthur'], 'Sir Lancelot');
   const card2 = new Card(2, 'What is your quest?', ['To go to the store', 'To eat cheese', 'I seek the Grail'], 'I seek the Grail');
   const card3 = new Card(3, 'What is your favorite color?', ['Blue', 'Yellow', 'Red'], 'Yellow');
-  const deck = new Deck([card1, card2, card3]);
+  const newDeck = new Deck([card1, card2, card3]);
 
   describe('Attributes', function() {
     it('should store a deck', function() {
-      const round = new Round(deck)
+      const round = new Round(newDeck)
       expect(round.deck.cards).to.deep.equal([card1, card2, card3])
     })
 
     it('should store a turn counter', function() {
-      const round = new Round(deck)
+      const round = new Round(newDeck)
       expect(round.turns).to.deep.equal(0)
     })
 
     it('should store a list of incorrect guesses', function() {
-      const round = new Round(deck)
+      const round = new Round(newDeck)
       expect(round.incorrectGuesses).to.deep.equal([])
     })
   })
 
   describe('takeTurn', function() {
-    xit('should instantiate a new turn', function() {
-      const round = new Round(deck)
-      expect(round.takeTurn()).to.be.an.instanceof(Turn)
-    })
 
-    xit('should update turn counter at the end of a turn', function() {
-      const round = new Round(deck)
+    it('should update turn counter at the end of a turn', function() {
+      const newDeck = new Deck([card1, card2, card3]);
+      const round = new Round(newDeck)
       round.takeTurn()
       expect(round.turns).to.deep.equal(1)
     })
 
-    xit('should turn the next card into the current card', function() {
-      const round = new Round(deck)
+    it('should turn the next card into the current card', function() {
+      const newDeck = new Deck([card1, card2, card3]);
+      const round = new Round(newDeck)
       round.takeTurn('Sir Lancelot')
       expect(round.returnCurrentCard()).to.deep.equal(card2)
     })
 
-    xit('should store incorrect guesses in an array', function() {
-      const round = new Round(deck)
+    it('should store incorrect guesses in an array', function() {
+      const newDeck = new Deck([card1, card2, card3]);
+      const round = new Round(newDeck)
       round.takeTurn('King Arthur')
       expect(round.incorrectGuesses[0]).to.deep.equal(card1)
     })
 
-    xit('should return feedback after given an answer', function() {
-      const round = new Round(deck)
+    it('should return feedback after given an answer', function() {
+      const newDeck = new Deck([card1, card2, card3]);
+      const round = new Round(newDeck)
       expect(round.takeTurn('Sir Lancelot')).to.deep.equal('correct!')
       expect(round.takeTurn('To go to the store')).to.deep.equal('incorrect!')
     })
