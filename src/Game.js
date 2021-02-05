46ca28ca467d39377ed826e302d207a8708bc9f9
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-02-04 18:08:37 -0600

    Fix syntax, remove unnecessary lines

--- src/Game.js
+++ src/Game.js
@@ -12,7 +12,7 @@ class Game {
   }
 
   currentRound() {
-    return this.round
+    return this.round;
   }
 
   start() {
--- test/Game-test.js
+++ test/Game-test.js
@@ -9,33 +9,21 @@ describe('Game', function() {
 
   it('should keep track of current round', function() {
     const game = new Game();
-    expect(game.currentRound()).to.deep.equal(game.round)
+    expect(game.currentRound()).to.deep.equal(game.round);
   })
 
   describe('Start', function() {
     it('should create cards and put them in the deck', function() {
-      const game = new Game()
-      game.start()
-      console.log(game.currentRound())
-      expect((game.currentRound()).deck.cards.length).to.deep.equal(30)
+      const game = new Game();
+      game.start();
+      expect((game.currentRound()).deck.cards.length).to.deep.equal(30);
     })
 
     it('should create a new round using deck', function() {
-      const game = new Game()
-      game.start()
-      expect((game.currentRound()).deck).to.deep.equal(game.deck)
-    })
-
-    it('should invoke PrintMessage', function() {
-      const game = new Game()
-      game.start()
-    })
-
-    it('should invoke printQuestion', function() {
-      const game = new Game()
-      game.start()
+      const game = new Game();
+      game.start();
+      expect((game.currentRound()).deck).to.deep.equal(game.deck);
     })
 
   })
-
 })
